e675bc003c9de6af2824cd61c87414f97b4e0d08
josephseo <josephseo.korea@gmail.com>
2021-03-08 16:12:51 +0900

    d
    d

--- app.py
+++ app.py
@@ -35,6 +35,30 @@ def course():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 �
 def tip():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
     return render_template('tip.html')
 
+@app.route('/tip/etiquette')
+def etiquette():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
+    return render_template('etiquette.html')
+
+@app.route('/tip/equipment')
+def equipment():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
+    return render_template('equipment.html')
+
+@app.route('/tip/ready')
+def ready():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
+    return render_template('ready.html')
+
+@app.route('/tip/pick')
+def pick():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
+    return render_template('pick.html')
+
+@app.route('/tip/start')
+def start():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
+    return render_template('start.html')
+
+@app.route('/tip/safety')
+def safety():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
+    return render_template('safety.html')
+
 # 산 name으로 산 정보 검색
 @app.route('/find_mt', methods=['GET'])
 def find_mt():
